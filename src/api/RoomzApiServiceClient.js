01677d363ccd085971a1b8115bf5db95b7ef7e5f
ABFranco <abfranco4@gmail.com>
2021-04-19 23:22:24 -0700

    Fixed tabs in api client

--- src/api/RoomzApiServiceClient.js
+++ src/api/RoomzApiServiceClient.js
@@ -25,240 +25,239 @@ enableDevTools([
 
 
 function createAccount(data) {
-    var request = new CreateAccountRequest();
-    request.setFirstName(data['firstName']);
-    request.setLastName(data['lastName']);
-    request.setEmail(data['email']);
-    request.setPassword(data['password']);
-    
-    return new Promise((resolve, reject) => {
-        client.createAccount(request, {}, (error, response) => {
-            if (response) {
-                resolve(response);
-            } else if (error) {
-                reject(error);
-            } else {
-                reject(null);
-            }
-        })
-    });
+  var request = new CreateAccountRequest();
+  request.setFirstName(data['firstName']);
+  request.setLastName(data['lastName']);
+  request.setEmail(data['email']);
+  request.setPassword(data['password']);
+  
+  return new Promise((resolve, reject) => {
+    client.createAccount(request, {}, (error, response) => {
+      if (response) {
+        resolve(response);
+      } else if (error) {
+        reject(error);
+      } else {
+        reject(null);
+      }
+    })
+  });
 }
 
 function signIn(data) {
-    var request = new SignInRequest();
-    request.setEmail(data['email']);
-    request.setPassword(data['password']);
-    
-    return new Promise((resolve, reject) => {
-        client.signIn(request, {}, (error, response) => {
-            if (response) {
-                resolve(response);
-            } else if (error) {
-                reject(error);
-            } else {
-                reject(null);
-            }
-        });
+  var request = new SignInRequest();
+  request.setEmail(data['email']);
+  request.setPassword(data['password']);
+  
+  return new Promise((resolve, reject) => {
+    client.signIn(request, {}, (error, response) => {
+      if (response) {
+        resolve(response);
+      } else if (error) {
+        reject(error);
+      } else {
+        reject(null);
+      }
     });
+  });
 }
 
 function createRoom(data) {
-    var request = new CreateRoomRequest();
-    request.setUserId(data['userId']);
-    request.setUserName(data['userName']);
-    request.setPassword(data['password']);
-    request.setIsStrict(data['isStrict']);
+  var request = new CreateRoomRequest();
+  request.setUserId(data['userId']);
+  request.setUserName(data['userName']);
+  request.setPassword(data['password']);
+  request.setIsStrict(data['isStrict']);
 
-    return new Promise((resolve, reject) => {
-        client.createRoom(request, {}, (error, response) => {
-            if (response) {
-                resolve(response);
-            } else if (error) {
-                reject(error);
-            } else {
-                reject(null);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    client.createRoom(request, {}, (error, response) => {
+      if (response) {
+        resolve(response);
+      } else if (error) {
+        reject(error);
+      } else {
+        reject(null);
+      }
     });
+  });
 }
 
 function closeRoom(data) {
-    var request = new CloseRoomRequest();
-    request.setRoomId(data['roomId']);
+  var request = new CloseRoomRequest();
+  request.setRoomId(data['roomId']);
 
-    return new Promise((resolve, reject) => {
-        client.closeRoom(request, {}, (error, response) => {
-            if (response) {
-                resolve(response);
-            } else if (error) {
-                reject(error);
-            } else {
-                reject(null);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    client.closeRoom(request, {}, (error, response) => {
+      if (response) {
+        resolve(response);
+      } else if (error) {
+        reject(error);
+      } else {
+        reject(null);
+      }
     });
+  });
 }
 
 function awaitRoomClosure(data) {
-    var request = new AwaitRoomClosureRequest();
-    request.setRoomId(data['roomId']);
-    request.setUserId(data['userId']);
-    request.setToken(data['token']);
+  var request = new AwaitRoomClosureRequest();
+  request.setRoomId(data['roomId']);
+  request.setUserId(data['userId']);
+  request.setToken(data['token']);
 
-    return new Promise((resolve, reject) => {
-        const closureStream = client.awaitRoomClosure(request, {});
+  return new Promise((resolve, reject) => {
+    const closureStream = client.awaitRoomClosure(request, {});
 
-        if (closureStream) {
-            resolve(closureStream);
-        } else {
-            reject(null);
-        }
-    });
+    if (closureStream) {
+      resolve(closureStream);
+    } else {
+      reject(null);
+    }
+  });
 }
 
 function joinRoom(data) {
-    var request = new JoinRoomRequest();
-    request.setRoomId(data['roomId']);
-    request.setRoomPassword(data['roomPassword']);
-    request.setUserId(data['userId']);
-    request.setUserName(data['userName']);
-    request.setIsGuest(data['isGuest']);
+  var request = new JoinRoomRequest();
+  request.setRoomId(data['roomId']);
+  request.setRoomPassword(data['roomPassword']);
+  request.setUserId(data['userId']);
+  request.setUserName(data['userName']);
+  request.setIsGuest(data['isGuest']);
 
-    return new Promise((resolve, reject) => {
-        // TODO: figure out how to obtain error if joinRoom fails
-        const chatStream = client.joinRoom(request, {});
-        if (chatStream) {
-            resolve(chatStream);
-        } else {
-            reject(null);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    const chatStream = client.joinRoom(request, {});
+    if (chatStream) {
+      resolve(chatStream);
+    } else {
+      reject(null);
+    }
+  });
 }
 
 function leaveRoom(data) {
-    var request = new LeaveRoomRequest();
-    request.setRoomId(data['roomId']);
-    request.setUserId(data['userId']);
+  var request = new LeaveRoomRequest();
+  request.setRoomId(data['roomId']);
+  request.setUserId(data['userId']);
 
-    return new Promise((resolve, reject) => {
-        client.leaveRoom(request, {}, (error, response) => {
-            if (response) {
-                resolve(response);
-            } else if (error) {
-                reject(error);
-            } else {
-                reject(null);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    client.leaveRoom(request, {}, (error, response) => {
+      if (response) {
+        resolve(response);
+      } else if (error) {
+        reject(error);
+      } else {
+        reject(null);
+      }
     });
+  });
 }
 
 function enterChatRoom(data) {
-    var request = new EnterChatRoomRequest();
-    request.setRoomId(data['roomId']);
-    request.setUserId(data['userId']);
-    request.setToken(data['token']);
+  var request = new EnterChatRoomRequest();
+  request.setRoomId(data['roomId']);
+  request.setUserId(data['userId']);
+  request.setToken(data['token']);
 
-    return new Promise((resolve, reject) => {
-        // Note: chatStream is a StreamInterceptor object, not a payload of data
-        const chatStream = client.enterChatRoom(request, {});
+  return new Promise((resolve, reject) => {
+    // Note: chatStream is a StreamInterceptor object, not a payload of data
+    const chatStream = client.enterChatRoom(request, {});
 
-        if (chatStream) {
-            resolve(chatStream);
-        } else {
-            reject(null);
-        }
-    });
+    if (chatStream) {
+      resolve(chatStream);
+    } else {
+      reject(null);
+    }
+  });
 }
 
 function chatMessage(data) {
-    var request = new ChatMessage();
-    request.setRoomId(data['roomId']);
-    request.setUserId(data['userId']);
-    request.setUserName(data['username']);
-    request.setMessage(data['message']);
-    request.setTimestamp(data['timestamp']);
+  var request = new ChatMessage();
+  request.setRoomId(data['roomId']);
+  request.setUserId(data['userId']);
+  request.setUserName(data['username']);
+  request.setMessage(data['message']);
+  request.setTimestamp(data['timestamp']);
 
-    return new Promise((resolve, reject) => {
-        client.sendChatMessage(request, {}, (error, response) => {
-            if (response) {
-                resolve(response);
-            } else if (error) {
-                reject(error);
-            } else {
-                reject(null);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    client.sendChatMessage(request, {}, (error, response) => {
+      if (response) {
+        resolve(response);
+      } else if (error) {
+        reject(error);
+      } else {
+        reject(null);
+      }
     });
+  });
 }
 
 function getJoinRequests(data) {
-    var request = new GetJoinRequestsRequest();
-    request.setRoomId(data['roomId']);
-    request.setUserId(data['userId']);
+  var request = new GetJoinRequestsRequest();
+  request.setRoomId(data['roomId']);
+  request.setUserId(data['userId']);
 
-    return new Promise((resolve, reject) => {
-        client.getJoinRequests(request, {}, (error, response) => {
-            if (response) {
-                resolve(response);
-            } else if (error) {
-                reject(error);
-            } else {
-                reject(null);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    client.getJoinRequests(request, {}, (error, response) => {
+      if (response) {
+        resolve(response);
+      } else if (error) {
+        reject(error);
+      } else {
+        reject(null);
+      }
     });
+  });
 }
 
 function handleJoinRequest(data) {
-    var request = new HandleJoinRequestRequest();
-    request.setRoomId(data['roomId']);
-    request.setUserIdToHandle(data['userIdToHandle']);
-    request.setDecision(data['decision']);
+  var request = new HandleJoinRequestRequest();
+  request.setRoomId(data['roomId']);
+  request.setUserIdToHandle(data['userIdToHandle']);
+  request.setDecision(data['decision']);
 
-    return new Promise((resolve, reject) => {
-        client.handleJoinRequest(request, {}, (error, response) => {
-            if (response) {
-                resolve(response);
-            } else if (error) {
-                reject(error);
-            } else {
-                reject(null);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    client.handleJoinRequest(request, {}, (error, response) => {
+      if (response) {
+        resolve(response);
+      } else if (error) {
+        reject(error);
+      } else {
+        reject(null);
+      }
     });
+  });
 }
 
 function cancelJoinRequest(data) {
-    var request = new CancelJoinRequestRequest();
-    request.setRoomId(data['roomId']);
-    request.setUserId(data['userId']);
+  var request = new CancelJoinRequestRequest();
+  request.setRoomId(data['roomId']);
+  request.setUserId(data['userId']);
 
-    return new Promise((resolve, reject) => {
-        client.cancelJoinRequest(request, {}, (error, response) => {
-            if (response) {
-                resolve(response);
-            } else if (error) {
-                reject(error);
-            } else {
-                reject(null);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    client.cancelJoinRequest(request, {}, (error, response) => {
+      if (response) {
+        resolve(response);
+      } else if (error) {
+        reject(error);
+      } else {
+        reject(null);
+      }
     });
+  });
 }
 
 
 export {
-    createAccount,
-    signIn,
-    createRoom,
-    closeRoom,
-    awaitRoomClosure,
-    joinRoom,
-    leaveRoom,
-    enterChatRoom,
-    chatMessage,
-    getJoinRequests,
-    handleJoinRequest,
-    cancelJoinRequest
+  createAccount,
+  signIn,
+  createRoom,
+  closeRoom,
+  awaitRoomClosure,
+  joinRoom,
+  leaveRoom,
+  enterChatRoom,
+  chatMessage,
+  getJoinRequests,
+  handleJoinRequest,
+  cancelJoinRequest
 }
