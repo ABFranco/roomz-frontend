36393e7ceae3e6644529580fb876b7891fd40b1b
ABFranco <abfranco4@gmail.com>
2021-05-11 22:37:50 -0700

    Split Vestibule into MediaPreview and VestibulePanel

---
+++ src/components/Vestibule/MediaPreview/MediaPreview.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './MediaPreview.css';
+
+function MediaPreview() {
+
+  return (
+    <div className="media-preview">
+      <div className="media-preview-placeholder"></div>
+    </div>
+  );
+}
+
+export default MediaPreview;
